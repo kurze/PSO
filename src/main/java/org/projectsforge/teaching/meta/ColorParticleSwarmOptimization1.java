53ea8e60dde68b1450e6a0a9c3884d24771b34a5
Simon Kesteloot <simon@kesteloot.me>
2013-12-17 10:28:28 +0100

    improve default parameters and example parameters

--- src/main/java/org/projectsforge/teaching/meta/ColorParticleSwarmOptimization1.java
+++ src/main/java/org/projectsforge/teaching/meta/ColorParticleSwarmOptimization1.java
@@ -13,15 +13,18 @@ public class ColorParticleSwarmOptimization1 {
         final String[] myArgs = new String[]{
                 "-problem", "org.projectsforge.teaching.meta.colorproblem.problem.ColorProblem",
                 "-instance", "10-1",
-                //"-maxevaluations", "10000",
-                "-runCount", "8",
+                "-maxevaluations", "20000",
+                "-runCount", "20",
                 "-solver", "org.projectsforge.teaching.meta.colorproblem.solver.ParticleSwarmOptimization.ColorParticleSwarmOptimizationSearch",
                 "-output", "swarm-10-1",
                 "-detailedoutput", "false",
-                "-momentum", "0.8",
-                "-bestLocalWeight", "0.5",
-                "-bestGlobalWeight", "0.9",
-                "-nbParticles", "10000"
+                "-momentum", "2",
+                "-bestLocalWeight", "0.8",
+                "-bestGlobalWeight", "1.4",
+                "-nbParticles", "1000",
+                "-decreaseRate", "5",
+                "-decrease", "true"
+
         };
 
         Master.run(myArgs);
--- src/main/java/org/projectsforge/teaching/meta/colorproblem/solver/ParticleSwarmOptimization/ColorParticleSwarmOptimizationSearch.java
+++ src/main/java/org/projectsforge/teaching/meta/colorproblem/solver/ParticleSwarmOptimization/ColorParticleSwarmOptimizationSearch.java
@@ -11,21 +11,27 @@ public class ColorParticleSwarmOptimizationSearch implements Solver {
 
     private Problem problem;
 
+    // parameter for normal particle swarm optimization
     @Parameter(names = "-momentum", description = "inertia strength of particles")
-    public double momentum = 1;
+    public double momentum = 0.8;
 
     @Parameter(names = "-bestLocalWeight", description = "weight of the best local solution")
-    public double bestLocalWeight = 1;
+    public double bestLocalWeight = 0.5;
 
     @Parameter(names = "-bestGlobalWeight", description = "weight of the best global solution")
-    public double bestGlobalWeight = 1;
+    public double bestGlobalWeight = 0.9;
 
     @Parameter(names = "-nbParticles", description = "number of particles")
-    public int nbParticles = 100;
+    public int nbParticles = 10000;
 
-    /**
-     * The improvement mark.
-     */
+    // parameter for decrease addition
+    @Parameter(names = "-decrease", description = "activation of decease addition")
+    public boolean decrease = true;
+
+    @Parameter(names = "-decreaseRate", description = "rate of decrease of other parameters")
+    public double decreaseRate = 3;
+
+    // parameter for display
     @Parameter(names = "-improvementmark", description = "Improvement mark")
     public String improvementMark = ".";
 
@@ -51,9 +57,11 @@ public class ColorParticleSwarmOptimizationSearch implements Solver {
                 System.out.print(improvementMark);
             }
         }
+        int iii = 0;
         System.out.print("!");
         // run
         while (!problem.shouldStop()) {
+            iii++;
 
             for (int i = 0; i < nbParticles; i++) {
                 particles[i].calculateNewSpeed(momentum, bestLocalWeight, bestGlobalWeight, best);
@@ -63,8 +71,13 @@ public class ColorParticleSwarmOptimizationSearch implements Solver {
                     System.err.print(improvementMark);
                 }
             }
+            if (decrease) {
+                bestGlobalWeight = bestGlobalWeight - (bestGlobalWeight * 100 / decreaseRate);
+                bestLocalWeight = bestLocalWeight - (bestLocalWeight * 100 / decreaseRate);
+                momentum = momentum - (momentum * 100 / decreaseRate);
+            }
         }
-        System.out.println();
+        System.out.println(iii);
     }
 
     @Override
